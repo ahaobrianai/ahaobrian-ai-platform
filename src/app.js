ae4459f496913147e1d547a5b9fbf831579d7f84
ahaobrianai <ahaobrian486@gmail.com>
2025-10-17 14:31:24 +0300

    Add basic React component for the app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function App() {
+  return (
+    <div>
+      <h1>Hello, AhaobrianAI!</h1>
+      <p>Your dashboard app is starting up. More features coming soon!</p>
+    </div>
+  );
+}
+
+export default App;
